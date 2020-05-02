56b850bfac5f4a1429a207e538dc0e155240242f
verma-priyanka <pverm157@gmail.com>
2020-05-02 18:35:57 -0400

    Date Columns Converted

--- data_raw/sheets.R
+++ data_raw/sheets.R
@@ -1,20 +1,26 @@
-# Download updated data from google sheets
-
 # install.packages('gsheet')
 library(gsheet)
 
-# read data as table from google sheets
-twitter <- gsheet2tbl('https://docs.google.com/spreadsheets/d/15s_bO2TgGo4KPJX8ExNPjluI8E_1Ur8e_IWTde_a2NE/edit#gid=665301748')
+# --DATA LOAD--
+# From Google Sheets
+# master table with interventions collected by county
 master <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1ccGuWTmiX35REcr_JOFOCixNf-GIPII2jS76qxwO2kY/edit#gid=0')
-
-# mobility data
+twitter <- gsheet2tbl('https://docs.google.com/spreadsheets/d/15s_bO2TgGo4KPJX8ExNPjluI8E_1Ur8e_IWTde_a2NE/edit#gid=665301748')
+# mobility data downloaded from google (as of April 9)
 mobility <- read_csv("./data/Global_Mobility_Report.csv")
+
+# --DATA CONVERSION--
+# convert date columns for final data load
+county_interventions <- master %>% filter(!is.na(SAH_County_Date))
+county_interventions$SAH_County_Date = as.Date(county_interventions$SAH_County_Date,
+                                               format = '%m/%d/%Y')
+county_interventions$SAH_State_Date = as.Date(county_interventions$SAH_State_Date,
+                                               format = '%m/%d/%Y')
 mobility$date = as.Date(mobility$date, format = '%m/%d/%Y')
 # filter data to county level data only
 mobility <- mobility %>% filter(!is.na(sub_region_2))
-county_interventions <- master %>% filter(!is.na(SAH_County_Date))
 
-# Save as .rds
+# --SAVE AS R OBJECT--
 usethis::use_data(twitter, overwrite = TRUE)
 usethis::use_data(county_interventions, overwrite = TRUE)
 usethis::use_data(mobility, overwrite = TRUE)
