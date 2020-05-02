0210a88c47df270f45eb0521fde75c5deba0a4f2
verma-priyanka <pverm157@gmail.com>
2020-05-02 16:13:21 -0400

    Analysis Vignette

--- data_raw/sheets.R
+++ data_raw/sheets.R
@@ -5,17 +5,17 @@ library(gsheet)
 
 # read data as table from google sheets
 twitter <- gsheet2tbl('https://docs.google.com/spreadsheets/d/15s_bO2TgGo4KPJX8ExNPjluI8E_1Ur8e_IWTde_a2NE/edit#gid=665301748')
-# master <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1qg5SG9dqgJof7dYQREGEhLYgLhd2hApm1i7jkbrkStk/edit#gid=0')
+master <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1ccGuWTmiX35REcr_JOFOCixNf-GIPII2jS76qxwO2kY/edit#gid=0')
 
 # mobility data
 mobility <- read_csv("./data/Global_Mobility_Report.csv")
 mobility$date = as.Date(mobility$date, format = '%m/%d/%Y')
 # filter data to county level data only
 mobility <- mobility %>% filter(!is.na(sub_region_2))
-
+county_interventions <- master %>% filter(!is.na(SAH_County_Date))
 
 # Save as .rds
 usethis::use_data(twitter, overwrite = TRUE)
-# usethis::use_data(master, overwrite = TRUE)
+usethis::use_data(county_interventions, overwrite = TRUE)
 usethis::use_data(mobility, overwrite = TRUE)
 
